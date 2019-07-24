b04a42f918ed99788f600dee0d448484069a67dc
Hache <hriolfo@yahoo.com>
2019-07-23 23:36:11 -0300

    Bug

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email', Length(min=4, max=50)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(min=4, max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
