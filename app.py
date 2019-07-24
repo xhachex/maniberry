1f7596d5c7b0360033169702fc0e10e9ba58f168
Hache <hriolfo@yahoo.com>
2019-07-23 23:22:30 -0300

    Error

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'S3cr3t'
 Bootstrap(app)
+
+### Class ###
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+### Routes ###
 
 @app.route('/')
 def index():
@@ -10,7 +23,8 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @app.route('/signup')
 def signup():
